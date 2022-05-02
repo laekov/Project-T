adba625923db15cffb9c030296b2662464452efb
laekov <rivenhe@foxmail.com>
2022-05-02 18:12:33 +0800

    init frontend

---
+++ README.md
@@ -0,0 +1,4 @@
+Project-T
+===
+
+A collection of MTB sites in Tsinghua University (or any other places that AMap works).
---
+++ frontend/app.js
@@ -0,0 +1,41 @@
+var map = 0;
+
+$(document).ready(function() {
+	map = new AMap.Map('map_container', {
+		resizeEnable: true,
+		zoom: 16,
+		center: [116.32281, 40.003519],
+	});
+
+	var ctx_menu = new AMap.ContextMenu();
+	var evt_coor = 0;
+	ctx_menu.addItem("添加一个点", () => {
+		// var pixel = map.lngLatToContainer(evt_coor);
+		// console.log(pixel);
+		$('#addform').show();
+		console.log(evt_coor);
+		var infownd = new AMap.InfoWindow({
+			content: $('#addform')[0]
+		});
+		infownd.open(map, evt_coor);
+	});
+
+	map.on('rightclick', (e) => {
+		evt_coor = e.lnglat;
+		ctx_menu.open(map, e.lnglat);
+	});
+	$('#addform').hide();
+
+	$('#btn-submit').click(() => {
+		var frm = {
+			lat: evt_coor.Q,
+			lng: evt_coor.R,
+			name: $('#sitename').val(),
+			type: $('#sitetype').val(),
+			diff: $('#sitediff').val(),
+			desc: $('#sitedesc').val(),
+			author: $('#contributor').val()
+		};
+		console.log(frm);
+	});
+});
